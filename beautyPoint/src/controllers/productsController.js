5fa21e2a607c048b93ac812231315ebfdc917bb2
maurocds <mauro.carvajaldesousa@gmail.com>
2022-05-09 00:47:42 -0300

    Subo primera version del formulario de create de un producto impactando en memoria. Quité el id de producto del formulario (antes de crear un producto levanto el último id existente en memoria y le sumo 1. Redirige a la home una vez creado. Tiene console logs para ver el flujo

--- beautyPoint/src/controllers/productsController.js
+++ beautyPoint/src/controllers/productsController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
@@ -23,11 +23,36 @@ const productsController = {
   // Create - Render del formulario de creación de un producto
   create: (req, res) => {
     console.log('entrando al método create del productController.js');
-    res.status(200).render('./products/create', { products: products });
+    res.status(200).render('./products/create');
   },
   // Create -  Método que persiste la data del formulario de creación de un producto
   store: (req, res) => {
     console.log('Entró al método store del productController.js');
+
+    // Calculo id del producto a crear (será 1 más que el último en base de datos)
+    // Obs: Version tempo hasta implementar base de datos.
+    let idCalculadoDelProductoNuevo = products.reduce(
+      (idNuevo, producto) =>
+        (idNuevo =
+          idNuevo > parseInt(producto.id) ? idNuevo : parseInt(producto.id)),
+      0
+    );
+    idCalculadoDelProductoNuevo++;
+    console.log('id calculado para nuevo prod: ' + idCalculadoDelProductoNuevo);
+    let productoNuevo = {
+      id: idCalculadoDelProductoNuevo,
+      name: req.body.product_name,
+      image: req.body.product_image,
+      price: req.body.product_price,
+      description: req.body.product_description,
+      package: req.body.product_package,
+      category: req.body.product_category,
+    };
+
+    products.push(productoNuevo);
+    // res.send(productoNuevo);
+    console.log('Producto creado OK!');
+    res.redirect('/');
   },
 
   // Edit - Render del formulario de edición de un producto
