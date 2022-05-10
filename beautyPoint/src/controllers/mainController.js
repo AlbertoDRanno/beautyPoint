9e8c434ea6a4c954aef6f7cf8396f4512f9eba9c
AlbertoRanno <albertoranno@hotmail.com>
2022-05-10 16:49:06 -0300

    barra de busqueda funcional - results.ejs - json con 50 elementos mockeados

--- beautyPoint/src/controllers/mainController.js
+++ beautyPoint/src/controllers/mainController.js
@@ -9,10 +9,31 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const mainController = {
   index: (req, res) => {
     console.log("entrando al render de index");
-    let cuidadosBasicos = products.filter((product) => product.category == "Cuidados Básicos");
+    let cuidadosBasicos = products.filter(
+      (product) => product.category == "Cuidados Básicos"
+    );
     let antiage = products.filter((product) => product.category == "Antiage");
-    res.status(200).render("index", { cuidadosBasicos, antiage, toThousand });
+let solares = products.filter((product) => product.category == "Solares");
+let maquillajes = products.filter(
+  (product) => product.category == "Maquillajes"
+);
+    res.status(200).render("index", { cuidadosBasicos, antiage, solares, maquillajes, toThousand });
   },
-}
+  search: (req, res) => {
+    let loQueBuscoElUsuario = req.query.keywords.toLowerCase();
+    console.log(loQueBuscoElUsuario);
+    let productsResults = [];
+    for (let i = 0; i < products.length; i++) {
+      console.log("entró al FOR");
+      if (products[i].name.toLowerCase().includes(loQueBuscoElUsuario)) {
+        productsResults.push(products[i]);
+      }
+    }
+    res.status(200).render("results", {
+      productsResults,
+      toThousand,
+    });
+  },
+};
       
 module.exports = mainController;
