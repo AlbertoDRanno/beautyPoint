10746bcf56c4db09b74a616353ebbb4016f0095d
AlbertoRanno <albertoranno@hotmail.com>
2022-05-11 19:00:00 -0300

    multer en create

--- beautyPoint/src/controllers/productsController.js
+++ beautyPoint/src/controllers/productsController.js
@@ -1,64 +1,70 @@
-const JsonModel = require('../models/jsonModel');
-const productsModel = new JsonModel('products');
+const JsonModel = require("../models/jsonModel");
+const productsModel = new JsonModel("products");
 
 const productsController = {
   // Detail - Detalle de un producto a partir de su id
   detail: (req, res) => {
-    console.log('entrando al render detail de productsController.js');
+    console.log("entrando al render detail de productsController.js");
     let product = productsModel.buscar(req.params.id);
 
     if (product) {
-      res.render('./products/detail', { products: product });
+      res.render("./products/detail", { products: product });
     } else {
-      res.render('./not-found');
+      res.render("./not-found");
     }
   },
   // Create - Render del formulario de creación de un producto
   create: (req, res) => {
-    console.log('entrando al método create del productController.js');
-    res.status(200).render('./products/create');
+    console.log("entrando al método create del productController.js");
+    res.status(200).render("./products/create");
   },
   // Create -  Método que persiste la data del formulario de creación de un producto
   store: (req, res) => {
-    console.log('Entró al método store del productController.js');
-    req.body.image = '/images/products/prueba.jpg';
-    let productId = productsModel.save(req.body);
-    res.redirect('/products/detail/' + productId);
-  },
+    console.log("Entró al método store del productController.js");
+    let imageFile = req.file;
+    if (imageFile !== undefined) {
+      console.log(req.file);
+      req.body.image = "/images/products/" + req.file.filename;
+      //req.body.image = "/images/products/prueba.jpg";
+      let productId = productsModel.save(req.body);
+      res.redirect("/products/detail/" + productId);
+    } else {
+      return res.send("No enviaste ninguna imagen para tu producto");
+    }},
 
   // Edit - Render del formulario de edición de un producto
   edit: (req, res) => {
-    console.log('Entró al método edit del productController.js');
+    console.log("Entró al método edit del productController.js");
     let product = productsModel.buscar(req.params.id);
-
     if (product) {
-      res.render('./products/edit', { products: product });
+      
+      res.render("./products/edit", { products: product });
       console.log(product);
     } else {
-      res.render('./not-found');
+      res.render("./not-found");
     }
   },
   // Update - Method to update
   update: (req, res) => {
-    console.log('Entró al método update del productController.js');
+    console.log("Entró al método update del productController.js");
 
     let productoAeditar = productsModel.buscar(req.params.id);
     req.body.id = req.params.id;
     req.body.image = productoAeditar.image;
     // console.log(req.body);
     productsModel.update(req.body);
-    res.redirect('/');
+    res.redirect("/");
   },
   cart: (req, res) => {
-    console.log('entrando al render cart');
-    res.status(200).render('./products/cart');
+    console.log("entrando al render cart");
+    res.status(200).render("./products/cart");
   },
   // Delete - Delete one product from DB
   destroy: (req, res) => {
-    console.log('Entró al método destroy del productController.js');
+    console.log("Entró al método destroy del productController.js");
 
     productsModel.destroy(req.params.id);
-    res.redirect('/');
+    res.redirect("/");
   },
 };
 
--- beautyPoint/src/routes/products.js
+++ beautyPoint/src/routes/products.js
@@ -1,11 +1,26 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
+const path = require("path");
+
+// ************ Multer ************
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let folder = path.join(__dirname, "../../public/images/products");
+    cb(null, folder); 
+  }, 
+  filename: function (req, file, cb) {
+    console.log(file); 
+    let imageName = "product-" + Date.now() + path.extname(file.originalname); 
+    cb(null, imageName); 
+  },
+});
+const uploadFile = multer({ storage });
 
 // ************ Controller Require ************
 const productsController = require('../controllers/productsController.js');
 
-// ************ WRITE YOUR CODE FROM HERE ************
 // ************ methods() ************
 
 /*** GET ONE PRODUCT ***/
@@ -13,11 +28,11 @@ router.get('/detail/:id', productsController.detail);
 
 /*** CREATE ONE PRODUCT ***/
 router.get('/create', productsController.create);
-router.post('/create', productsController.store);
+router.post('/create', uploadFile.single("image"), productsController.store);
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/edit/:id', productsController.edit);
-router.put('/edit/:id', productsController.update);
+router.put('/edit/:id', uploadFile.single("image"), productsController.update);
 
 /*** DELETE ONE PRODUCT***/
 router.delete('/delete/:id', productsController.destroy);
