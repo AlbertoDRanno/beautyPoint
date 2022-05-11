58575a4df0fe104af62cbe61c268de26f072d2a3
maurocds <mauro.carvajaldesousa@gmail.com>
2022-05-11 12:50:56 -0300

    Subo primera version del modelo de datos + crud andando. Hay que readaptar vistas, html y css...

--- beautyPoint/src/controllers/productsController.js
+++ beautyPoint/src/controllers/productsController.js
@@ -1,24 +1,17 @@
-const fs = require('fs');
-const path = require('path');
-
-const productsFilePath = path.join(__dirname, '../data/products.json');
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+const JsonModel = require('../models/jsonModel');
+const productsModel = new JsonModel('products');
 
 const productsController = {
   // Detail - Detalle de un producto a partir de su id
   detail: (req, res) => {
     console.log('entrando al render detail de productsController.js');
-    let filteredProduct = products.filter(
-      (product) => product.id == req.params.id
-    );
-    console.log(filteredProduct);
+    let product = productsModel.buscar(req.params.id);
 
-    res.status(200).render('./products/detail', {
-      theFiltered: filteredProduct[0],
-      toThousand,
-    });
+    if (product) {
+      res.render('./products/detail', { products: product });
+    } else {
+      res.render('./not-found');
+    }
   },
   // Create - Render del formulario de creación de un producto
   create: (req, res) => {
@@ -28,58 +21,44 @@ const productsController = {
   // Create -  Método que persiste la data del formulario de creación de un producto
   store: (req, res) => {
     console.log('Entró al método store del productController.js');
-
-    // Calculo id del producto a crear (será 1 más que el último en base de datos)
-    // Obs: Version temporal hasta implementar base de datos. Solo crea el proximo.
-    let idCalculadoDelProductoNuevo = products.reduce(
-      (idNuevo, producto) =>
-        (idNuevo =
-          idNuevo > parseInt(producto.id) ? idNuevo : parseInt(producto.id)),
-      0
-    );
-    idCalculadoDelProductoNuevo++;
-    console.log('id calculado para nuevo prod: ' + idCalculadoDelProductoNuevo);
-    let productoNuevo = {
-      id: idCalculadoDelProductoNuevo,
-      name: req.body.product_name,
-      image: req.body.product_image,
-      price: req.body.product_price,
-      description: req.body.product_description,
-      package: req.body.product_package,
-      category: req.body.product_category,
-    };
-
-    console.log(productoNuevo);
-    products.push(productoNuevo);
-
-    res.send(productoNuevo);
-    console.log('Producto creado OK!');
-    console.log('Productos actualizados: ');
-    console.log(products);
-    // res.redirect('/');
+    req.body.image = '/images/products/prueba.jpg';
+    let productId = productsModel.save(req.body);
+    res.redirect('/products/detail/' + productId);
   },
 
   // Edit - Render del formulario de edición de un producto
   edit: (req, res) => {
     console.log('Entró al método edit del productController.js');
-    res.status(200).render('./products/edit');
-    // res.status(200).render('./products/edit', { products: productoEditable[0] });
+    let product = productsModel.buscar(req.params.id);
+
+    if (product) {
+      res.render('./products/edit', { products: product });
+      console.log(product);
+    } else {
+      res.render('./not-found');
+    }
   },
   // Update - Method to update
   update: (req, res) => {
     console.log('Entró al método update del productController.js');
+
+    let productoAeditar = productsModel.buscar(req.params.id);
+    req.body.id = req.params.id;
+    req.body.image = productoAeditar.image;
+    // console.log(req.body);
+    productsModel.update(req.body);
+    res.redirect('/');
   },
   cart: (req, res) => {
     console.log('entrando al render cart');
     res.status(200).render('./products/cart');
   },
-  // productList: (req, res) => {
-  //   console.log('entrando al render de productList');
-  //   res.status(200).render('');
-  // },
   // Delete - Delete one product from DB
   destroy: (req, res) => {
     console.log('Entró al método destroy del productController.js');
+
+    productsModel.destroy(req.params.id);
+    res.redirect('/');
   },
 };
 
---
+++ beautyPoint/src/models/jsonModel.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const path = require('path');
+
+class JsonModel {
+  constructor(name) {
+    this.name = name;
+    this.dataDir = '../data/';
+    this.dataPath = path.resolve(__dirname, this.dataDir, this.name + '.json');
+  }
+
+  /** Lee el archivo JSON */
+  readJsonFile() {
+    let fileContents = fs.readFileSync(this.dataPath, 'utf-8');
+    if (fileContents) {
+      return JSON.parse(fileContents);
+    }
+    return [];
+  }
+
+  /** Escribe el archivo JSON */
+  writeJsonFile(data) {
+    let jsonData = JSON.stringify(data, null, ' ');
+    fs.writeFileSync(this.dataPath, jsonData);
+  }
+
+  /** Genera el próximo valor para la pk */
+  generatePk() {
+    let items = this.readJsonFile();
+    let lastItem = items.pop();
+
+    if (lastItem) {
+      return ++lastItem.id;
+    }
+
+    return 1;
+  }
+
+  /** Trae todos los registros */
+  all() {
+    return this.readJsonFile();
+  }
+
+  /** Trae un registro por su valor de pk */
+  buscar(id) {
+    let items = this.readJsonFile();
+    return items.find((item) => item.id == id);
+  }
+
+  /** Filtra registros por su clave y valor */
+  filtrarPorCampoValor(campo, valor) {
+    let items = this.readJsonFile();
+    return items.filter((item) => item[campo] == valor);
+  }
+
+  /** Guarda el registro en la colección */
+  save(item) {
+    let items = this.readJsonFile();
+    item.id = this.generatePk();
+    items.push(item);
+
+    this.writeJsonFile(items);
+
+    return item.id;
+  }
+
+  /** Actualiza el registro de la colección */
+  update(item) {
+    let items = this.readJsonFile();
+
+    let updatedItems = items.map((currentItem) => {
+      if (currentItem.id == item.id) {
+        return (currentItem = item);
+      }
+      return currentItem;
+    });
+
+    this.writeJsonFile(updatedItems);
+
+    return item.id;
+  }
+
+  /** Elimina el registro de la colección */
+  destroy(id) {
+    let items = this.readJsonFile();
+
+    let filteredItems = items.filter((currentItem) => currentItem.id != id);
+
+    this.writeJsonFile(filteredItems);
+  }
+}
+
+module.exports = JsonModel;
