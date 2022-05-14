41a125069ca3930a5a1c4131a93bf0293f9fa23e
AlbertoRanno <albertoranno@hotmail.com>
2022-05-14 11:30:25 -0300

    index arreglado

--- beautyPoint/src/controllers/mainController.js
+++ beautyPoint/src/controllers/mainController.js
@@ -1,21 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-const productsFilePath = path.join(__dirname, "../data/products.json");
-const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+// const productsFilePath = path.join(__dirname, "../data/products.json");
+// const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const JsonModel = require("../models/jsonModel");
+const productsModel = new JsonModel("products");
 
 const mainController = {
   index: (req, res) => {
     console.log("entrando al render de index");
+    let products = productsModel.readJsonFile();
+    //console.log(products)
     let cuidadosBasicos = products.filter(
-      (product) => product.category == "Cuidados Básicos"
+      (product) => product.category == "categoria-1"
     );
-    let antiage = products.filter((product) => product.category == "Antiage");
-let solares = products.filter((product) => product.category == "Solares");
+    let antiage = products.filter(
+      (product) => product.category == "categoria-2"
+    );
+let solares = products.filter((product) => product.category == "categoria-3");
 let maquillajes = products.filter(
-  (product) => product.category == "Maquillajes"
+  (product) => product.category == "categoria-4"
 );
     res.status(200).render("index", { cuidadosBasicos, antiage, solares, maquillajes, toThousand });
   },
