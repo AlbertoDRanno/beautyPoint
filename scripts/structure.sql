22df7931d50b4b95fed8797e346b41968e4eb1a9
Mauro Carvajal de Sousa <MauroCarvajalDeSousa@users.noreply.github.com>
2022-06-13 17:08:59 -0300

    Update structure.sql
    Actualizo script

--- scripts/structure.sql
+++ scripts/structure.sql
@@ -1,21 +1,5 @@
 CREATE DATABASE `laca_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-CREATE TABLE `cart` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  `quantity` decimal(7,0) unsigned NOT NULL,
-  `price` float NOT NULL,
-  `createdAt` timestamp NULL DEFAULT NULL,
-  `updatedAt` timestamp NULL DEFAULT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_userCart_idx` (`user_id`),
-  KEY `fk_productCart_idx` (`product_id`),
-  CONSTRAINT `fk_productCart` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `fk_userCart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE `categories` (
   `id` int(10) unsigned NOT NULL,
   `description` varchar(50) NOT NULL,
@@ -54,6 +38,37 @@ CREATE TABLE `products` (
   CONSTRAINT `fk_productPackage` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `dni` int(10) unsigned NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `genero` tinyint(1) NOT NULL,
+  `avatar` varchar(400) NOT NULL,
+  `password` varchar(400) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `cart` (
+  `id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` decimal(7,0) unsigned NOT NULL,
+  `price` float NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userCart_idx` (`user_id`),
+  KEY `fk_productCart_idx` (`product_id`),
+  CONSTRAINT `fk_productCart` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `fk_userCart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `publications` (
   `id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
@@ -68,19 +83,4 @@ CREATE TABLE `publications` (
   KEY `fk_userPublication_idx` (`user_id`),
   CONSTRAINT `fk_productPublication` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
   CONSTRAINT `fk_userPublication` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `dni` int(10) unsigned NOT NULL,
-  `email` varchar(320) NOT NULL,
-  `genero` tinyint(1) NOT NULL,
-  `avatar` varchar(400) NOT NULL,
-  `password` varchar(400) NOT NULL,
-  `createdAt` timestamp NULL DEFAULT NULL,
-  `updatedAt` timestamp NULL DEFAULT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
