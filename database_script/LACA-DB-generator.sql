fb5f9ff022ccd6abdc2daf59faac0469bdd9c5f3
Mauro Carvajal de Sousa <MauroCarvajalDeSousa@users.noreply.github.com>
2022-06-13 08:53:32 -0300

    Update LACA-DB-generator.sql
    Agrego columnas para baja lógica de registros. Corregí error campo description (lo pasé desde tabla publicaciones a tabla productos).

--- database_script/LACA-DB-generator.sql
+++ database_script/LACA-DB-generator.sql
@@ -1,28 +1,44 @@
 CREATE DATABASE `laca_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
+CREATE TABLE `cart` (
+  `id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `quantity` decimal(7,0) unsigned NOT NULL,
+  `price` float NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userCart_idx` (`user_id`),
+  KEY `fk_productCart_idx` (`product_id`),
+  CONSTRAINT `fk_productCart` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `fk_userCart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `categories` (
   `id` int(10) unsigned NOT NULL,
   `description` varchar(50) NOT NULL,
   `createdAt` timestamp NULL DEFAULT NULL,
   `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 CREATE TABLE `packages` (
   `id` int(10) unsigned NOT NULL,
   `description` varchar(50) NOT NULL,
   `createdAt` timestamp NULL DEFAULT NULL,
   `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
 CREATE TABLE `products` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `price` float unsigned NOT NULL,
+  `description` text NOT NULL,
   `discount` decimal(4,2) unsigned DEFAULT NULL,
   `package_id` int(10) unsigned NOT NULL,
   `category_id` int(10) unsigned NOT NULL,
@@ -30,6 +46,7 @@ CREATE TABLE `products` (
   `stock` decimal(7,0) unsigned NOT NULL,
   `createdAt` timestamp NULL DEFAULT NULL,
   `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_productCategory_idx` (`category_id`),
   KEY `fk_productPackage_idx` (`package_id`),
@@ -37,6 +54,21 @@ CREATE TABLE `products` (
   CONSTRAINT `fk_productPackage` FOREIGN KEY (`package_id`) REFERENCES `packages` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `publications` (
+  `id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `price` float unsigned NOT NULL,
+  `quantity` decimal(7,0) unsigned NOT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_productPublication_idx` (`product_id`),
+  KEY `fk_userPublication_idx` (`user_id`),
+  CONSTRAINT `fk_productPublication` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `fk_userPublication` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -49,38 +81,6 @@ CREATE TABLE `users` (
   `password` varchar(400) NOT NULL,
   `createdAt` timestamp NULL DEFAULT NULL,
   `updatedAt` timestamp NULL DEFAULT NULL,
+  `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE `cart` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  `quantity` decimal(7,0) unsigned NOT NULL,
-  `price` float NOT NULL,
-  `createdAt` timestamp NULL DEFAULT NULL,
-  `updatedAt` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_userCart_idx` (`user_id`),
-  KEY `fk_productCart_idx` (`product_id`),
-  CONSTRAINT `fk_productCart` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `fk_userCart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
-CREATE TABLE `publications` (
-  `id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `product_id` int(10) unsigned NOT NULL,
-  `description` text NOT NULL,
-  `price` float unsigned NOT NULL,
-  `quantity` decimal(7,0) unsigned NOT NULL,
-  `createdAt` timestamp NULL DEFAULT NULL,
-  `updatedAt` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_productPublication_idx` (`product_id`),
-  KEY `fk_userPublication_idx` (`user_id`),
-  CONSTRAINT `fk_productPublication` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `fk_userPublication` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
