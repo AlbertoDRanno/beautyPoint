5281b269c02e18d49988ce2d99383fb4636aa76c
maurocds <mauro.carvajaldesousa@gmail.com>
2022-08-08 19:01:06 -0300

    Agregado de comments pass defaults en data.sql

--- scripts/data.sql
+++ scripts/data.sql
@@ -28,6 +28,7 @@ UNLOCK TABLES;
 
 /* en proceso: users y products... */
 /* INSERTS EN TABLA: users */
+/* password default: '12345678' */
 LOCK TABLES `users` WRITE;
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `dni`, `email`, `categoria`, `avatar`, `password`, `createdAt`, `updatedAt`, `status`, `phone`) VALUES
 (1, 'Rick', 'Sanchez', 20123456, 'rick@laca.com.ar', 1, '/images/dataset/avatar_prueba_01.jpg', '$2a$10$RLvCG1TvBVfgc.tXaTdbVe/1wD7/mlk1dMPhhI.SIWOQYemmTteDG', NULL, NULL, 1, '1234-5678');
